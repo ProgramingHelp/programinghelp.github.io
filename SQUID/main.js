43e17163a91f5a75d10c335d75b7a0f11109b783
ProgramingHelp <168842384+ProgramingHelp@users.noreply.github.com>
2025-02-25 21:40:03 +0200

    Adding the additional directory for s. squid game

---
+++ SQUID/main.js
@@ -0,0 +1,22 @@
+let PressedButtons = {button1: false, button2: false, button3: false};
+var audio = new Audio('sounds/call.m4a');
+function Check()
+{
+	if (PressedButtons.button1 && PressedButtons.button2 && PressedButtons.button3)
+	{
+		audio.play();
+	}
+}
+
+document.getElementById("1").addEventListener("click", function() {
+	PressedButtons.button1 = true;
+	Check();
+});
+document.getElementById("2").addEventListener("click", function() {
+	PressedButtons.button2 = true;
+	Check();
+});
+document.getElementById("3").addEventListener("click", function() {
+	PressedButtons.button3 = true;
+	Check();
+});
